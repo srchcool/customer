35dc9a3028a3b2a45d804be44cae0182bddcc2b4
Roumen Petrov <systerminator@gmail.com>
2018-06-08 13:12:08 -0400

    Added a test XML to JSON conversion for the data passing

--- create.js
+++ create.js
@@ -1,15 +1,19 @@
-import uuid from "uuid";
 import { crud, models } from '@welbot/sdk';
+import xml2jsonobj from './xml2jsonobj';
 
 //TODO: include this into '@welbot/sdk' as part of models
-var customer = require('./customer');
+import customer from './customer';
 models.customer = customer;
 //END TODO
 
 const { createBase, transferCallDataToDynamoRequestParameters } = crud.dynamoCreate;
 
 const customerDefinition = models.customer;
+
 const customerCreateParams = (baseParams, data, id) => {
+  
+  //Assuming the XML is in data['body']
+  data['body'] = JSON.stringify(xml2jsonobj(data['body']));
   const params = transferCallDataToDynamoRequestParameters(baseParams, data, customerDefinition);
   params.TableName = 'customer';
   params.Item.id = id;
