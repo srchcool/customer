2b267fe29866157f7374048f89dc61241ed5b4d9
Roumen Petrov <systerminator@gmail.com>
2018-06-13 10:15:33 +0100

    Cleaned up validation function calls.

--- customer.js
+++ customer.js
@@ -91,76 +91,76 @@ function customer(){
 	};	
 
 	const detailsSchema = ()=> {
-	  return( (0, objectSchema)().keys({
-	    	salutation: (0, stringSchema)().optional(),
-	    	firstName: (0, stringSchema)().required(),
-	    	lastName: (0, stringSchema)().required(),
-			email: (0, emailSchema)().required()
+	  return( objectSchema().keys({
+	    	salutation: stringSchema().optional(),
+	    	firstName: stringSchema().required(),
+	    	lastName: stringSchema().required(),
+			email: emailSchema().required()
 	    }).required() ); 
 	};
 
 	const discountSchema = ()=> {
-		return( (0, objectSchema)().keys({
-				discountType: (0, stringSchema)(),
-				discountPercentage: (0, numberSchema)()
+		return( objectSchema().keys({
+				discountType: stringSchema(),
+				discountPercentage: numberSchema()
 				}).optional() ); 
 		};
 
 	//TODO: is id here a uuid??	gateway-id??							  			
 	const paymentSchema = ()=> {
-		return( (0, objectSchema)().keys({
-				id: (0, stringSchema)(),
-				gatewayId: (0, stringSchema)(),
-				object: (0, stringSchema)()
+		return( objectSchema().keys({
+				id: stringSchema(),
+				gatewayId: stringSchema(),
+				object: stringSchema()
 				}).required() ); 
 		};	
 
 	const affiliateDiscountSchema = ()=> {
-		return( (0, objectSchema)().keys({
-				affiliateCode: (0, stringSchema)(),
-				discountId: (0, stringSchema)(),
-				discountPercentage: (0, numberSchema)()
+		return( objectSchema().keys({
+				affiliateCode: stringSchema(),
+				discountId: stringSchema(),
+				discountPercentage: numberSchema()
 				}).optional() ); 
 		};									 				
 
 	const affiliateSchema = ()=> {
-		return( (0, objectSchema)().keys({
-				affiliateType: (0, stringSchema)(),
-				affiliateId: (0, stringSchema)(),
-				discount: (0, affiliateDiscountSchema)()
+		return( objectSchema().keys({
+				affiliateType: stringSchema(),
+				affiliateId: stringSchema(),
+				discount: affiliateDiscountSchema()
 				}).optional() ); 
 		};	
 
 	const accountSchema = ()=> {
-		return( (0, objectSchema)().keys({
+		return( objectSchema().keys({
 
-				accountNumber: (0, stringSchema)().required(),
-				accountType: (0, stringSchema)().required(),
-				accountStart: (0, dateSchema)().required(),
-				pricePerSeat: (0, numberSchema)().required(),
-				monthlyPayment: (0, numberSchema)().required(),
-				yearlyPayment: (0, numberSchema)().required(),
-				discount: (0, discountSchema)().optional(),
-				affiliateCode: (0, stringSchema)().optional(),
-				licences: (0, numberSchema)().required(),
-				subscriptionPeriod: (0, stringSchema)().required(),
-				customerType: (0, stringSchema)().required(),
-				cancellation: (0, stringSchema)().required(),
-				refund: (0, stringSchema)().optional(),
-				refundAmount:(0, numberSchema)().optional(),	
-				payment: (0, paymentSchema)().required(),	
-				affiliate: (0, affiliateSchema)().optional()						
+				accountNumber: stringSchema().required(),
+				accountType: stringSchema().required(),
+				accountStart: dateSchema().required(),
+				pricePerSeat: numberSchema().required(),
+				monthlyPayment: numberSchema().required(),
+				yearlyPayment: numberSchema().required(),
+				discount: discountSchema().optional(),
+				affiliateCode: stringSchema().optional(),
+				licences: numberSchema().required(),
+				subscriptionPeriod: stringSchema().required(),
+				customerType: stringSchema().required(),
+				cancellation: stringSchema().required(),
+				refund: stringSchema().optional(),
+				refundAmount:numberSchema().optional(),	
+				payment: paymentSchema().required(),	
+				affiliate: affiliateSchema().optional()						
 
 			}).required() ); 
 	};	
 
 	const customerValidationSchema = {
 		  body: {
-					id: (0, uuidSchema)(), 
-					details: (0,detailsSchema)().required(),
-					userId: (0, uuidSchema)().required(),
-					organisationId: (0, uuidSchema)().required(),
-					account: (0,accountSchema)().required()
+					id: uuidSchema(), 
+					details: detailsSchema().required(),
+					userId: uuidSchema().required(),
+					organisationId: uuidSchema().required(),
+					account: accountSchema().required()
 		  }
 	};
 
